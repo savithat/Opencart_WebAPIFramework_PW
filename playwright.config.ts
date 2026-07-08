70d818979b15dd366642d450da1af8a9445242fe
savithat <savitha_tm@yahoo.com>
2026-07-08 16:41:39 -0700

    added github workflows sharded-tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,16 +20,30 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? '50%' : undefined,
+ 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: [
-    ["list"],
-    ["html", { outputFolder: "reports/html-report", open: "never" }],
-    ["allure-playwright", {
-      outputFolder: "allure-results",
-      suiteTitle: true,
-    }],
-  ],
+  //   reporter: [
+  //   ["list"],
+  //   ["html", { outputFolder: "reports/html-report", open: "never" }],
+  //   ["allure-playwright", {
+  //     outputFolder: "allure-results",
+  //     suiteTitle: true,
+  //   }],
+  // ],
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: process.env.CI
+    ? [
+      ["blob"],
+      ["html", { outputFolder: "reports/html-report", open: "never" }],
+      ["allure-playwright", { outputFolder: "allure-results", suiteTitle: true }],
+    ]
+    : [
+      ["list"],
+      ["html", { outputFolder: "reports/html-report", open: "never" }],
+      ["allure-playwright", { outputFolder: "allure-results", suiteTitle: true }],
+    ],
+  
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: process.env.BASE_URL,
